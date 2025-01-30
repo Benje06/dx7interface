401a60997c69f5765d3fb8668441b00f6690d4ee
Jerome <benhaim_jerome@yahoo.fr>
2025-01-30 21:20:05 +0100

    correct typo

--- dx7interface/src/dx7interface.h
+++ dx7interface/src/dx7interface.h
@@ -43,7 +43,7 @@
 
 /** CONSTANTS **/
 //#define UI MOD_UI_DIRECTORY"dx7interface-0.0.1-simplify.ui"
-#define UI MOD_UI_DIRECTORY"dx7interface-0.0.1-gtk4_git-simpilfy.ui"
+#define UI MOD_UI_DIRECTORY"dx7interface-0.0.1-gtk4_git-simplify.ui"
 #define CSSFILE MOD_UI_DIRECTORY"theme.css"
 
 extern "C" {
