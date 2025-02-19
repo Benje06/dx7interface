d5d53af6aed4bdd24ef02601ae504cf2a5ec8167
Jerome <benhaim_jerome@yahoo.fr>
2025-02-19 22:20:49 +0100

    replace g type to std type, rename struct
    to fit number of elements in not the max value

--- dx7interface/src/dx7sysex.h
+++ dx7interface/src/dx7sysex.h
@@ -18,130 +18,137 @@
     #ifndef dx7sysex_H
         #define dx7sysex_H
 
-    typedef struct st_1{
-        guint8 val;
-        guint address;
-        static const guint8 max=1;
-        static const guint8 mask=0x01;
-    }St_1;
+    typedef struct st_2{
+        uint8_t val;
+        uint address;
+        static const uint8_t max=1;
+        static const uint8_t mask=0x01;
+    }St_2;
 
-    typedef struct st_3{
-        guint8 val;
-        static const guint8 max=3;
-        static const guint8 mask=0x03;
-    }St_3;
+    typedef struct st_4{
+        uint8_t val;
+        static const uint8_t max=3;
+        static const uint8_t mask=0x03;
+    }St_4;
 
-    typedef struct st_5{
-        guint8 val;
-        static const guint8 max=5;
-        static const guint8 mask=0x07;
-    }St_5;
+    typedef struct st_6{
+        uint8_t val;
+        static const uint8_t max=5;
+        static const uint8_t mask=0x07;
+    }St_6;
 
-    typedef struct st_7{
-        guint8 val;
-        guint address;
-        static const guint8 max=7;
-        static const guint8 mask=0x07;
-    }St_7;
+    typedef struct st_8{
+        uint8_t val;
+        uint address;
+        static const uint8_t max=7;
+        static const uint8_t mask=0x07;
+    }St_8;
 
     typedef struct st_14{
-        guint8 val;
-        guint address;
-        static const guint8 max=14;
-        static const guint8 mask=0x0F;
+        uint8_t val;
+        uint address;
+        static const uint8_t max=13;
+        static const uint8_t mask=0x0F;
     }St_14;
 
-    typedef struct st_31{
-        guint8 val;
-        guint address;
-        static const guint8 max=31;
-        static const guint8 mask=0x1F;
-    }St_31;
+    typedef struct st_15{
+        uint8_t val;
+        uint address;
+        static const uint8_t max=14;
+        static const uint8_t mask=0x0F;
+    }St_15;
 
-    typedef struct st_48{
-        guint8 val;
-        guint address;
-        static const guint8 max=48;
-        static const guint8 mask=0x3F;
-    }St_48;
+    typedef struct st_32{
+        uint8_t val;
+        uint address;
+        static const uint8_t max=31;
+        static const uint8_t mask=0x1F;
+    }St_32;
 
-    typedef struct st_99{
-        guint8 val;
-        guint address;
-        static const guint8 max=99;
-        static const guint8 mask=0x7F;
-    }St_99;
+    typedef struct st_49{
+        uint8_t val;
+        uint address;
+        static const uint8_t max=48;
+        static const uint8_t mask=0x3F;
+    }St_49;
 
-    typedef struct st_127{
-        guint8 val;
-        guint address;
-        static const guint8 max=127;
-        static const guint8 mask=0x7F;
-    }St_127;
+    typedef struct st_100{
+        uint8_t val;
+        uint address;
+        static const uint8_t max=99;
+        static const uint8_t mask=0x7F;
+    }St_100;
+
+    typedef struct st_128{
+        uint8_t val;
+        uint address;
+        static const uint8_t max=127;
+        static const uint8_t mask=0x7F;
+    }St_128;
 
     typedef struct st_kls {
-        St_99 brk_pt;//  LEV SCL BRK PT 0-99
-        St_99 lft_dpth;//  SCL LEFT DEPTH 0-99
-        St_99 rght_dpth;//  SCL RGHT DEPTH 0-99
-        St_3 rght_curve;//  scl right curve 0-3
-        St_3 lft_curve;//  scl left curve 0-3
+        St_100 brk_pt;//  LEV SCL BRK PT 0-99
+        St_100 lft_dpth;//  SCL LEFT DEPTH 0-99
+        St_100 rght_dpth;//  SCL RGHT DEPTH 0-99
+        St_4 rght_curve;//  scl right curve 0-3
+        St_4 lft_curve;//  scl left curve 0-3
     }St_kls;
 
     typedef struct st_op {
-        St_99 eg_rt[4];//  op eg rate
-        St_99 eg_lvl[4];//  op eg level
-        St_99 dtun;// osc detune
-        St_7 krs;//  osc rate scale
-        St_7 kvs;// key vel sens
-        St_3 ams;// amp mod sens
-        St_99 lvl;//  output lev
-        St_31 freq_coarse;//  freq coarse
-        St_1 freq_mode;//   freq mode
-        St_99 freq_fine;//  freq fine
+        St_100 eg_rt[4];//  op eg rate
+        St_100 eg_lvl[4];//  op eg level
+        St_14 dtun;// osc detune
+        St_8 krs;//  osc rate scale
+        St_8 kvs;// key vel sens
+        St_4 ams;// amp mod sens
+        St_100 lvl;//  output lev
+        St_32 freq_coarse;//  freq coarse
+        St_2  freq_mode;//   freq mode
+        St_100 freq_fine;//  freq fine
         St_kls kls;
     }St_op;
 
     typedef struct st_pitch {
-        St_99 eg_rt[4];
-        St_99 eg_lvl[4];
+        St_100 eg_rt[4];
+        St_100 eg_lvl[4];
     }St_pitch;
 
     typedef struct st_lfo {
-        St_99 speed;
-        St_99 delay;
-        St_99 pmd;
-        St_99 amd;
-        St_7 pms;
-        St_5 wave;
-        St_1 sync;
+        St_100 speed;
+        St_100 delay;
+        St_100 pmd;
+        St_100 amd;
+        St_8 pms;
+        St_6 wave;
+        St_2 sync;
     }St_lfo;
 
     typedef struct st_algo {
-        St_31 algo;
-        St_1 oks;
-        St_7 feedback;
-        St_48 transpose;
+        St_32 algo;
+        St_2  oks;
+        St_8 feedback;
+        St_49 transpose;
     }St_algo;
 
     typedef struct st_funct {
-        St_1 poly_mono;
+        St_2  poly_mono;
         St_14 ptch_bnd_rng;
         St_14 ptch_bnd_stp;
-        St_1 portamento_md;
-        St_1 portamento_glss;
-        St_99 portamento_tm;
-        St_99 md_whl_rng;
-        St_7 md_whl_assgn;
-        St_99 foot_rng;
-        St_7 foot_assgn;
-        St_99 brth_rng;
-        St_7 brth_assgn;
-        St_99 aftrtch_rng;
-        St_7 aftrtch_assgn;
+        St_2  portamento_md;
+        St_2  portamento_glss;
+        St_100 portamento_tm;
+        St_100 md_whl_rng;
+        St_8 md_whl_assgn;
+        St_100 foot_rng;
+        St_8 foot_assgn;
+        St_100 brth_rng;
+        St_8 brth_assgn;
+        St_100 aftrtch_rng;
+        St_8 aftrtch_assgn;
     }St_funct;
 
     typedef struct st_extra {
-        St_127 mute;
+        St_128 mute;
         St_funct functions;
     }St_extra;
 
@@ -151,21 +158,13 @@
         St_algo algo;
         St_lfo lfo;
         Glib::ustring name;
-        guint8 sum;
+        uint8_t sum;
         St_extra extra;
     }St_dx7sysex_1;
 
-    template <guint i>
+    template <uint i>
     struct St_dx7sysex{
         St_dx7sysex_1 sound[i];
     };
 
-    /*typedef struct st_dx7sysex_32{
-        St_dx7sysex sound[32];
-    }St_dx7sysex_32;
-
-    typedef struct St_dx7sysex_128{
-        St_dx7sysex sound[128];
-    }St_dx7sysex_128;
-    */
 #endif /* dx7sysex_H */
