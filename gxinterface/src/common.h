737403be9ed11728b6ad14c50738708772e52b80
Jerome <benhaim_jerome@yahoo.fr>
2025-04-08 15:47:02 +0200

    Fix: str_to_ascii as inline

--- gxinterface/src/common.h
+++ gxinterface/src/common.h
@@ -106,7 +106,7 @@
         return strm.str();
     };
     /* convert glib::ustring to ascii-7bit */
-    constexpr std::string str_to_ascii(const Glib::ustring& input) {
+    inline std::string str_to_ascii(const Glib::ustring& input) {
         std::string result;
         for( auto ch : input ){
             if( ch < 128 ){ // Keep ASCII characters
